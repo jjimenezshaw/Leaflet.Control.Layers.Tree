0e8e991a58d255d566156c3e5500b58ee4411e08
Javier Jimenez Shaw <j1@jimenezshaw.com>
2017-07-28 17:56:34 +0200

    more functions, like collapseTree and expandTree

--- L.Control.Layers.Tree.js
+++ L.Control.Layers.Tree.js
@@ -6,12 +6,16 @@
         throw new Error('Leaflet must be included first');
     }
 
-
+    /*
+     * L.Control.Layers.Tree extends L.Control.Layers because it reuses
+     * most of its functionality. Only the HTML creation is different.
+     */
     L.Control.Layers.Tree = L.Control.Layers.extend({
         options: {
             closedSymbol: '+',
             openedSymbol: '&minus;',
             spaceSymbol: ' ',
+            selectorBack: false,
             namedToggle: false,
         },
 
@@ -20,7 +24,8 @@
             function iterate(tree, output, overlays) {
                 if (tree && tree.layer) {
                     if (!overlays) {
-                        tree.layer._layersTreeName = tree.name ? tree.name : tree.label;
+                        tree.layer._layersTreeName = tree.name ?
+                                                     tree.name : tree.label;
                     }
                     output[id++] = tree.layer;
                 }
@@ -31,6 +36,7 @@
                 }
                 return output;
             }
+
             this._baseTree = baseTree;
             this._overlaysTree = overlaysTree;
             L.Util.setOptions(this, options);
@@ -54,7 +60,6 @@
             console.log('removeLayer disabled for now');
         },
 
-
         onAdd: function(map) {
             function changeName(layer) {
                 if (layer._layersTreeName) {
@@ -63,19 +68,85 @@
             }
 
             var ret = L.Control.Layers.prototype.onAdd.call(this, map);
-            if (this.options.namedToggle) { 
+            if (this.options.namedToggle) {
                 var toggle = this._container.getElementsByClassName('leaflet-control-layers-toggle')[0];
                 L.DomUtil.addClass(toggle, 'leaflet-layerstree-named-toggle');
+                // Start with this value...
+                map.eachLayer(function(layer) {changeName(layer);});
+                // ... and change it whenever the baselayer is changed.
                 map.on('baselayerchange', function(e) {changeName(e.layer);}, this);
-                map.eachLayer(function(layer) { 
-                    if (layer._layersTreeName) changeName(layer);
-                });
             }
             return ret;
         },
 
+        expandTree: function(overlay) {
+            var container = overlay ? this._overlaysList : this._baseLayersList;
+            if (!container) return;
+            this._applyOnTree(container, false);
+        },
+
+        collapseTree: function(overlay) {
+            var container = overlay ? this._overlaysList : this._baseLayersList;
+            if (!container) return;
+            this._applyOnTree(container, true);
+        },
+
+        expandSelected: function(overlay) {
+            function iter(el) {
+                var p = el.parentElement;
+                if (p) {
+                    if (L.DomUtil.hasClass(p, 'leaflet-layerstree-children') &&
+                        !L.DomUtil.hasClass(el, 'leaflet-layerstree-children-nopad')) {
+                        L.DomUtil.removeClass(p, hide);
+                    }
+
+                    if (L.DomUtil.hasClass(p, 'leaflet-layerstree-node')) {
+                        var h = p.getElementsByClassName('leaflet-layerstree-header')[0];
+                        that._applyOnTree(h, false);
+                    }
+                    iter(p);
+                }
+            }
+
+            var that = this;
+            var container = overlay ? this._overlaysList : this._baseLayersList;
+            if (!container) return;
+            var hide = 'leaflet-layerstree-hide';
+            var inputs =  this._layerControlInputs || container.getElementsByTagName('input');
+            for (var i = 0; i < inputs.length; i++) {
+                var input = inputs[i];
+                if (this._getLayer && this._getLayer(input.layerId).overlay != overlay) continue
+                if (input.checked) {
+                    iter(input.parentElement.parentElement.parentElement.parentElement);
+                }
+            }
+        },
+
+        _applyOnTree: function(container, collapse) {
+            // collapses or expands the tree in the containter.
+            var iters = [
+                { cls: 'leaflet-layerstree-children', hide: collapse },
+                { cls: 'leaflet-layerstree-opened', hide: collapse },
+                { cls: 'leaflet-layerstree-closed', hide: !collapse },
+            ];
+            var hide = 'leaflet-layerstree-hide';
+            iters.forEach(function (it) {
+                var els = container.getElementsByClassName(it.cls);
+                for (var i = 0; i < els.length; i++) {
+                    var el = els[i];
+                    if (!L.DomUtil.hasClass(el, 'leaflet-layerstree-children-nopad')) {
+                        if (it.hide) {
+                            L.DomUtil.addClass(el, hide);
+                        } else {
+                            L.DomUtil.removeClass(el, hide);
+                        }
+                    }
+                }
+            });
+        },
+
         _addItem: function(obj) {
-            // it is called in the original _update, and should do nothing.
+            // it is called in the original _update, and shouldn't do anything.
         },
 
         /* overwrite _update function in Control.Layers */
@@ -92,7 +163,7 @@
             var container = overlay ? this._overlaysList : this._baseLayersList;
             this._iterateTreeLayout(tree, container, overlay, tree.noShow)
             if (this._checkDisabledLayers) {
-                // to keep compatibility with 0.7
+                // to keep compatibility
                 this._checkDisabledLayers();
             }
         },
@@ -107,14 +178,26 @@
                 if (append) append.appendChild(obj);
                 return obj;
             }
+
             var header = creator('div', 'leaflet-layerstree-header', container);
-            var sel = creator('span', null, header);
+            var sel = creator('span');
+            var entry = creator('span');
             var closed = creator('span', 'leaflet-layerstree-closed', sel, this.options.closedSymbol);
             var opened = creator('span', 'leaflet-layerstree-opened', sel, this.options.openedSymbol);
-            var space = creator('span', 'leaflet-layerstree-header-space', sel, this.options.spaceSymbol);
+            var space = creator('span', 'leaflet-layerstree-header-space', null, this.options.spaceSymbol);
+            if (this.options.selectorBack) {
+                sel.insertBefore(space, closed);
+                header.appendChild(entry);
+                header.appendChild(sel);
+            } else {
+                sel.appendChild(space);
+                header.appendChild(sel);
+                header.appendChild(entry);
+            }
 
-            var hide = 'leaflet-layerstree-hide';
-            if (tree.children && tree.children.length) {
+            var hide = 'leaflet-layerstree-hide'; // To toggle state
+            var neverShow = 'leaflet-layerstree-nevershow'; // To hide elements permanently
+            if (tree.children) {
                 var children = creator('div', 'leaflet-layerstree-children', container);
                 var sensible = tree.layer ? sel : header;
                 L.DomUtil.addClass(sensible, 'leaflet-layerstree-header-pointer');
@@ -136,10 +219,9 @@
                     that._iterateTreeLayout(child, node, overlay);
                 });
             } else {
-                L.DomUtil.addClass(sel, hide);
+                L.DomUtil.addClass(sel, neverShow);
             }
 
-            var entry = creator('span', null, header);
             var label = creator('label', 'leaflet-layerstree-header-label', entry);
             if (tree.layer) {
                 // now create the element like in _addItem
@@ -151,7 +233,7 @@
                     input = this._createRadioElement('leaflet-base-layers', checked);
                 }
                 if (this._layerControlInputs) {
-                    // to keep compatibility with 0.7
+                    // to keep compatibility with 1.0.3
                     this._layerControlInputs.push(input);
                 }
                 input.layerId = L.Util.stamp(tree.layer);
@@ -161,7 +243,7 @@
             var name = creator('span', 'leaflet-layerstree-header-name', label, tree.label);
             L.DomUtil.addClass(closed, hide);
             if (noShow) {
-                L.DomUtil.addClass(header, hide);
+                L.DomUtil.addClass(header, neverShow);
                 L.DomUtil.addClass(children, 'leaflet-layerstree-children-nopad');
             }
 
