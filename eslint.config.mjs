329609ef50239edf067d775879819daf8f3359de
obdulia-losantos <obdulia.losantos@gmail.com>
2025-01-19 12:16:32 +0100

    lint: use new eslint config file

---
+++ eslint.config.mjs
@@ -0,0 +1,65 @@
+import eslintJS from '@eslint/js';
+import eslintPluginHtml from 'eslint-plugin-html';
+import globals from 'globals';
+
+export default [
+    eslintJS.configs.recommended,
+    {
+        files: ['test/**'],
+        languageOptions: {
+            globals: {
+                ...globals.mocha,
+                ...globals.jest,
+                chai: true,
+                happen: true
+            }
+        },
+    },
+    {
+        files: ['**/*.js', '**/*.mjs', '**/*.html'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+                ...globals.es6,
+                L: true,
+            },
+        },
+        plugins: {
+            html: eslintPluginHtml,
+        },
+        settings: {
+            'html/indent': 'space',
+            'html/report-bad-indent': 2
+        },
+        rules: {
+            'camelcase': 'error',
+            'comma-spacing': ['error', {after: true}],
+            'comma-style': 'error',
+            'indent': 'error',
+            'key-spacing': 'error',
+            'keyword-spacing': 'error',
+            'no-console': 'error',
+            'no-constant-condition': 'off',
+            'no-lonely-if': 'error',
+            'no-multi-spaces': 'error',
+            'no-shadow': 'off',
+            'no-trailing-spaces': 'error',
+            'no-underscore-dangle': 'off',
+            'no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
+                },
+            ],
+            'object-curly-spacing': 'error',
+            'quotes': ['error', 'single', 'avoid-escape'],
+            'space-before-blocks': 'error',
+            'space-before-function-paren': ['error', 'never'],
+            'space-in-parens': 'error',
+            'strict': 'off'
+        },
+    },
+];
